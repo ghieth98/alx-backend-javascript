82eb197edc24b9b8e207cf6bf596defd1cbb1c3b
hossam ghieth <hossamghieth@gmail.com>
2024-01-06 04:13:16 +0200

    Create 4-pricing.js

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,38 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+    constructor(amount, currency) {
+        this._amount = amount;
+        this._currency = currency;
+    }
+
+    get amount() {
+        return this._amount;
+    }
+
+    set amount(amount) {
+        if (typeof amount !== 'number') {
+            throw TypeError('Amount must be a Number');
+        }
+        this._amount = amount;
+    }
+
+    get currency() {
+        return this._currency;
+    }
+
+    set currency(currency) {
+        if (!(currency instanceof Currency)) {
+            throw TypeError('Value must be a Currency');
+        }
+        this._currency = currency;
+    }
+
+    displayFullPrice() {
+        return `${this._amount} ${this._currency.displayFullCurrency()}`;
+    }
+
+    static convertPrice(amount, conversionRate) {
+        return amount * conversionRate;
+    }
+}
