daf2f25d5f919f07e7046a731ced7c7f53a7f6f2
hossam ghieth <hossamghieth@gmail.com>
2024-01-06 04:22:06 +0200

    Update 3-currency.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,18 +1,18 @@
 export default class Currency {
     constructor(code, name) {
-        this._code = code
-        this._name = name
+        this._code = code;
+        this._name = name;
     }
 
     get name() {
-        return this._name
+        return this._name;
     }
 
     set name(name) {
         if (typeof name !== 'string') {
             throw TypeError('name must be a string');
         }
-        this._name = name
+        this._name = name;
     }
 
     get code() {
@@ -21,9 +21,9 @@ export default class Currency {
 
     set code(code) {
         if (typeof code !== 'string') {
-            throw TypeError('code must be string');
+            throw TypeError('code must be a string');
         }
-        this._code = code
+        this._code = code;
     }
 
     displayFullCurrency() {
