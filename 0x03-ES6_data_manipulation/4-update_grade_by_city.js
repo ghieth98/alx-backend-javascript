310c00c67941c9ee414be8a4d5f1d43a26b4675c
hossam ghieth <hossamghieth@gmail.com>
2024-01-10 01:59:21 +0200

    Create 4-update_grade_by_city.js

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,20 @@
+function updateStudentGradeByCity(students, city, newGrades) {
+    return students
+        .filter((student) => student.location === city)
+        .map((student) => {
+            const matchingGrade = newGrades.find((grade) => grade.studentId === student.id);
+
+            if (matchingGrade) {
+                return {
+                    ...student,
+                    grade: matchingGrade.grade,
+                };
+            }
+            return {
+                ...student,
+                grade: 'N/A',
+            };
+        });
+}
+
+export default updateStudentGradeByCity;
