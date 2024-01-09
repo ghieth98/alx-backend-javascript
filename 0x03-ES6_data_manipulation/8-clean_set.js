8c05f85efea97d13c889d600b66166dbb2ec0114
hossam ghieth <hossamghieth@gmail.com>
2024-01-10 01:59:31 +0200

    Create 8-clean_set.js

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,17 @@
+function cleanSet(set, startString) {
+    if (typeof startString !== 'string' || startString === '') {
+        return '';
+    }
+
+    const result = [];
+
+    for (const item of set) {
+        if (typeof item === 'string' && item.startsWith(startString)) {
+            result.push(item.slice(startString.length));
+        }
+    }
+
+    return result.join('-');
+}
+
+export default cleanSet;
