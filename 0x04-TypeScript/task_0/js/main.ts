a24f1dd963d9710071c7543805130f80fd0fc2a0
hossam ghieth <hossamghieth@gmail.com>
2024-01-13 01:28:17 +0200

    Task 0

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,76 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: unknown;
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+console.log(teacher3);
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 98,
+};
+
+console.log(director1);
+
+// eslint-disable-next-line @typescript-eslint/class-name-casing
+interface printTeacherFunction {
+  (
+    firstName: string,
+    lastName: string
+  ): string;
+}
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`;
+}
+console.log(printTeacher("Colons", "Sam"));
+
+
+// class constructor
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentInstance;
+}
+
+interface StudentInstance {
+  firstName: string;
+  lastName: string;
+  workOnHomework: () => string;
+  displayName: () => string;
+}
+
+class StudentClass implements StudentInstance {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return `${this.firstName}`;
+  }
+}
+const classStudent = new StudentClass('Colons', 'Sam');
+console.log(classStudent.displayName());
+console.log(classStudent.workOnHomework());
