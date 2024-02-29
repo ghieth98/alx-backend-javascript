06dad11037a35c144ce58d39d61d376d90db4e59
hossam ghieth <hossamghieth@gmail.com>
2024-02-29 23:29:14 +0200

    Update 5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -11,7 +11,7 @@ const app = http.createServer((req, res) => {
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    res.write('This is the list of our students\n');
+    res.write('This is the list of our students');
     students(process.argv[2]).then((data) => {
       res.write(`Number of students: ${data.students.length}\n`);
       res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
